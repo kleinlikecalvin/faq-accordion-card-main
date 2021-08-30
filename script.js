f94bb8ed4ad855ad98b531245b04886c86b20c8b
streetlightkids <LTJ_HQ@HOTMAIL.COM>
2021-08-30 11:47:28 -0700

    trying to not mess up stuff

--- script.js
+++ script.js
@@ -13,20 +13,21 @@ question4.addEventListener("click", showAnswer4);
 
 
 function showAnswer0(){
-    const faqItem = document.getElementById("faq-item0");
-    const question = question0;
-    const answer = document.getElementById("faq-a0");
-    const icon = document.getElementById("arrow-icon0");
+    var faqItem = document.getElementById("faq-item0");
+    var question = question0;
+    var answer = document.getElementById("faq-a0");
+    var icon = document.getElementById("arrow-icon0");
 
     if(answer.style.visibility == "visible"){
-        answer.style.visibility = "hidden";
         faqItem.style.borderBottom = "1px solid var(--light-grey)";
+        answer.style.visibility = "hidden";
         question.style.fontWeight = "400";
         answer.style.position = "absolute";
         answer.style.borderBottom = "none";
         answer.style.paddingBottom = "0";
         icon.style.transform = "unset";
         icon.style.transition = "0.4s";
+
     } else {
         faqItem.style.borderBottom = "none";
         question.style.fontWeight = "700";
@@ -42,10 +43,10 @@ function showAnswer0(){
 }
 
 function showAnswer1(){
-    const faqItem = document.getElementById("faq-item1");
-    const question = question1;
-    const answer = document.getElementById("faq-a1");
-    const icon = document.getElementById("arrow-icon1");
+    var faqItem = document.getElementById("faq-item1");
+    var question = question1;
+    var answer = document.getElementById("faq-a1");
+    var icon = document.getElementById("arrow-icon1");
 
 
     if(answer.style.visibility == "visible"){
@@ -72,10 +73,10 @@ function showAnswer1(){
 }
 
 function showAnswer2(){
-    const faqItem = document.getElementById("faq-item2");
-    const question = question2;
-    const answer = document.getElementById("faq-a2");
-    const icon = document.getElementById("arrow-icon2");
+    var faqItem = document.getElementById("faq-item2");
+    var question = question2;
+    var answer = document.getElementById("faq-a2");
+    var icon = document.getElementById("arrow-icon2");
 
 
     if(answer.style.visibility == "visible"){
@@ -102,40 +103,40 @@ function showAnswer2(){
 }
 
 function showAnswer3(){
-    const faqItem = document.getElementById("faq-item3");
-    const question = question3;
-    const answer = document.getElementById("faq-a3");
-    const icon = document.getElementById("arrow-icon3");
+    var faqItem = document.getElementById("faq-item3");
+    var question = question3;
+    var answer = document.getElementById("faq-a3");
+    var icon = document.getElementById("arrow-icon3");
 
 
     if(answer.style.visibility == "visible"){
-        answer.style.visibility = "hidden";
         faqItem.style.borderBottom = "1px solid var(--light-grey)";
-        question.style.fontWeight = "400";
+        question.style.fontWeight = "400";  
+        icon.style.transform = "unset";
+        icon.style.transition = "0.4s";     
+        answer.style.visibility = "hidden";
         answer.style.position = "absolute";
         answer.style.borderBottom = "none";
         answer.style.paddingBottom = "0";
-        icon.style.transform = "unset";
-        icon.style.transition = "0.4s";
     } else {
         faqItem.style.borderBottom = "none";
-        question.style.fontWeight = "700";
+        question.style.fontWeight = "700";        
+        icon.style.transform = "rotate(0.5turn)";
+        icon.style.transition = "0.4s";
         answer.style.visibility = "visible";
         answer.style.position = "static";
         answer.style.fontSize = "13px";
         answer.style.margin = "0";
         answer.style.borderBottom = "1px solid var(--light-grey)";
         answer.style.paddingBottom = "1em";
-        icon.style.transform = "rotate(0.5turn)";
-        icon.style.transition = "0.4s";
     }
 }
 
 function showAnswer4(){
-    const faqItem = document.getElementById("faq-item4");
-    const question = question4;
-    const answer = document.getElementById("faq-a4");
-    const icon = document.getElementById("arrow-icon4");
+    var faqItem = document.getElementById("faq-item4");
+    var question = question4;
+    var answer = document.getElementById("faq-a4");
+    var icon = document.getElementById("arrow-icon4");
 
 
     if(answer.style.visibility == "visible"){
